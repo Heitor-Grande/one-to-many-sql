d32ad9934ed6f8aa7c4b96e0d2ae6078a4fcf60f
Heitor-Grande <68671033+Heitor-Grande@users.noreply.github.com>
2023-03-21 14:55:47 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-//express config
+//express
 const express = require("express")
 const app = express()
 
-//server door
-const port = process.env.PORT || 8080
-app.listen(port, function(){
-    console.log("SERVER ON: " + port)
+//porta servidor
+app.listen("8080", function(){
+    console.log("rodando")
 })
 
 //body-parser
 const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended: false}))
 
-//database config
-const database = require("./models/models")
-
-
-//produto controllers
-const produto = require("./controllers/produto")
-    //select
-app.get("/", produto)
-    //insert
-app.post("/createProduto", produto)
-    //delete
-app.delete("/del/:id_produto", produto)
-
-
-//nota controller
-const nota = require("./controllers/nota")
-
-    //insert
-app.post("/AddNota/ToProduto/:id_produto", nota)
+//database
+const database = require("./models/database")
