e53212ed014b6579ccb8f3a2e92d64a464bc9627
Heitor-Grande <68671033+Heitor-Grande@users.noreply.github.com>
2023-03-25 16:37:44 -0300

    Delete models directory

--- models/database.js
+++
@@ -1,7 +0,0 @@
-//sqlite3
-const sqlite3 = require("sqlite3")
-const database = new sqlite3.Database("./models/onetomany.db", function(){
-    console.log("conectado ou criado")
-})
-
-module.exports = database
--- models/models.js
+++
@@ -1,7 +0,0 @@
-//database
-const sqlite3 = require("sqlite3")
-const database = new sqlite3.Database("./models/database.db", function(){
-    console.log("BANCO CRIADO/CONECTADO COM SUCESSO")
-})
-
-module.exports = database
